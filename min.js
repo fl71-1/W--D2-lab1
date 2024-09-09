a331d3f1afc27d4d3046d9c22ffdeec49b7e0f47
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-09 11:13:16 +0300

    lab1

---
+++ min.js
@@ -0,0 +1,18 @@
+for (let i = 25; i >= 0; i--) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+for(let i=1; i <= 10 ;i++){
+    let squer=i*i
+        console.log(squer)
+    
+}
+        
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
